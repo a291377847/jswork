0c76bdd1f326ea443c5e8116549f5c9017ec0cce
a291377847 <291377847@qq.com>
2019-10-17 10:00:45 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,50 @@
+
+function calculate(){
+    
+    let num = document.getElementById('num').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+     
+        document.getElementById('pai').value = pi(num)
+    }
+}
+
+function factorial(n){
+    
+    let sum = 1
+    
+    for(let i=1;i<=n;++i ){
+        sum += i;
+    }
+    
+    return sum
+}
+
+function oddFactorial(n){
+    
+    let sum = 1
+    
+    for(let i=1;i<=n;++i){
+        sum += i;
+    }
+    
+    return sum
+}
+
+function pi(num){
+    
+    let sum = 0
+    
+    for(let i=0;i<=num;++i){
+        
+        let dividend = factorial()
+        
+        let divisor = oddFactorial(i)
+    
+     
+    }
+    
+    return sum
+}
+
+calculate()
