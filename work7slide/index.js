03a9797635a9367d7e8fdcd5d42ff9158f1291bd
a291377847 <291377847@qq.com>
2019-10-21 18:02:39 +0800

    work7slide

--- work7slide/index.js
+++ work7slide/index.js
@@ -5,11 +5,10 @@ function sort(method){
     if(method=='bubble'){
         logs = bubbleSort(origin)
     }else if(method=='insert'){
-        logs = insertSort(origin)
+        logs = inserSort(origin)
     }
     showDiv = document.getElementById('Anim')
-logValues = logs.values()
-console.log(logValues.next())
-setTimeout("showLog(logValues,showDiv)",50)
+    logValues = logs.values()
+    console.log(logValues.next())
+    setTimeout("showLog(logValues,showDiv)", 50)
 }
-
