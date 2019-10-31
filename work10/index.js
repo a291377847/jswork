03f23338ede880451223a4d7d99213a11fe3ef32
a291377847 <291377847@qq.com>
2019-10-31 10:02:01 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,45 @@
+function monkey(){
+    let total = document.getElementById("monkeytotal").value
+    let kick = document.getElementById("monkeykick").value
+    total = parseInt(total)&&Number(total)
+    kick = parseInt(kick)&&Number(kick)
+    if(isNaN(total) ||isNaN(kick)){
+        alert('请输入数字')
+        return
+    }
+    let monkey=[]
+    for (let i=1;i<=total;i++){
+        monkey.push(i)
+    }
+    let i=0
+    while (monkey.length>1){
+        i++;
+        head = monkey.shift()
+            if(i%kick!=0){
+                monkey.push(head);
+            }
+        
+    }
+    document.getElementById('monkeyking').innerText = monkey[0]
+}
+
+
+
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    let strs = new Array();
+    strs=str.split(",")
+    for (i=0;i<strs.length ;i++ ) { 
+        
+      
+      
+    } 
+    
+    
+    document.getElementById('result').innerText = JSON.stringify(obj)
+        
+        
+    }
+
+
