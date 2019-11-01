66c51fa9bf15a61a9550961b1b6781be9429efb5
a291377847 <291377847@qq.com>
2019-11-01 23:05:30 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -1,24 +1,23 @@
 function monkey(){
-    let total = document.getElementById("monkeytotal").value
-    let kick = document.getElementById("monkeykick").value
-    total = parseInt(total)&&Number(total)
-    kick = parseInt(kick)&&Number(kick)
-    if(isNaN(total) ||isNaN(kick)){
+    let total =document.getElementById("monkeytotal").value
+    let kick =document.getElementById("monkeykick").value
+    total = parseInt(total) && Number(total)
+    kick = parseInt(kick) && Number(kick)
+    if(isNaN(total)||isNaN(kick)){
         alert('请输入数字')
         return
     }
     let monkey=[]
     for (let i=1;i<=total;i++){
-        monkey.push(i)
+          monkey.push(i)
     }
     let i=0
     while (monkey.length>1){
         i++;
         head = monkey.shift()
-            if(i%kick!=0){
-                monkey.push(head);
-            }
-        
+        if(i%kick!=0){
+           monkey.push(head); 
+        } 
     }
     document.getElementById('monkeyking').innerText = monkey[0]
 }
@@ -28,18 +27,13 @@ function monkey(){
 function stat(){
     let str = document.getElementById("str").value
     let obj = {}
-    let strs = new Array();
-    strs=str.split(",")
-    for (i=0;i<strs.length ;i++ ) { 
-        
-      
-      
-    } 
-    
-    
-    document.getElementById('result').innerText = JSON.stringify(obj)
-        
-        
-    }
-
+    let a = str.split("")
+    a = a.sort()
+    for (let i = 0; i < str.length; ++i) {
+      let cc = a[i]
+        obj[cc] = (obj [cc] + 1) || 1;
+            
+        }
+      document.getElementById('result').innerText = JSON.stringify(obj)
+}
 
